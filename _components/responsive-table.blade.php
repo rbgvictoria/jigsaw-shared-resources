14146238df8ce6013350a674e1ae8429a6924433
Niels Klazenga <niels.klazenga@rbg.vic.gov.au>
2026-05-02 08:37:08 +0000

    Set table styles and create responsive table component

--- _components/responsive-table.blade.php
+++ _components/responsive-table.blade.php
@@ -1,4 +1,4 @@
-<div class="mb-6 w-full overflow-x-auto border border-gray-200 rounded-lg shadow-sm dark:border-gray-700">
+<div class="mb-6 w-full overflow-x-auto border border-gray-200 dark:border-gray-700">
     <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
         {{ $slot }}
     </table>
