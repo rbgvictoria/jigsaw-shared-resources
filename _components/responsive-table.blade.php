841b6fcf5134aa04042b6ed2dac8acccddbdfb56
Niels Klazenga <niels.klazenga@rbg.vic.gov.au>
2026-05-02 05:42:04 +0000

    Create responsive table

---
+++ _components/responsive-table.blade.php
@@ -0,0 +1,5 @@
+<div class="mb-6 w-full overflow-x-auto border border-gray-200 rounded-lg shadow-sm dark:border-gray-700">
+    <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+        {{ $slot }}
+    </table>
+</div>
