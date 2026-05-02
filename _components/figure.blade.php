afb3177f8a04737da251563059da39d7faa0c1c2
Niels Klazenga <niels.klazenga@rbg.vic.gov.au>
2026-05-02 11:21:56 +0000

    Add fullWidth option to figure component

--- _components/figure.blade.php
+++ _components/figure.blade.php
@@ -1,3 +1,6 @@
+{{-- source/_shared/_components/figure.blade.php --}}
+@props(['src', 'alt' => '', 'fullWidth' => false])
+
 @php
     $container = \Illuminate\Container\Container::getInstance();
     $jigsawPage = $container->has('pageData') ? $container->make('pageData')->page : null;
@@ -5,14 +8,14 @@
     $baseUrl = $jigsawPage->baseUrl ?? '';
 @endphp
 
-<figure class="my-10 flex flex-col items-center">
-    <div class="w-fit max-w-full p-3 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-900 dark:border-gray-800">
+<figure class="my-10 flex flex-col items-center {{ $fullWidth ? 'w-full' : '' }}">
+    <div class="{{ $fullWidth ? 'w-full max-w-7xl' : 'w-fit max-w-full' }} p-3 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-900 dark:border-gray-800">
         
         {{-- The Image Wrapper --}}
         <div class="overflow-hidden rounded-lg">
             <img src="{{ rtrim($baseUrl, '/') }}/{{ ltrim($src, '/') }}" 
                  alt="{{ $alt ?? '' }}" 
-                 class="block mx-auto max-h-[500px] w-auto h-auto object-contain">
+                 class="block mx-auto {{ $fullWidth ? 'w-full' : 'max-h-[500px] w-auto' }} h-auto object-contain">
         </div>
 
         {{-- The Caption (Now inside the box) --}}
